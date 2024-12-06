6e10300747a7b2b5500a38b993e90858219fd524
Braian A. Diez <bdiez19@gmail.com>
2024-12-06 20:24:48 -0300

    feat: update zen and niri config

--- home/software/wayland/niri/config.nix
+++ home/software/wayland/niri/config.nix
@@ -16,6 +16,8 @@ in {
         QT_QPA_PLATFORM = "wayland";
         QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         NIXOS_OZONE_WL = "1";
+        # test env
+        DISPLAY = null;
       };
       spawn-at-startup = [
         (makeCommand "/usr/libexec/polkit-gnome-authentication-agent-1")
@@ -51,7 +53,7 @@ in {
           tap-button-map = "left-right-middle";
         };
         focus-follows-mouse.enable = true;
-        warp-mouse-to-focus = true;
+        # warp-mouse-to-focus = true;
         workspace-auto-back-and-forth = true;
       };
       outputs = {
@@ -87,16 +89,17 @@ in {
           active.gradient = {
             angle = 45;
             relative-to = "workspace-view";
-            from = "#9796f0";
-            to = "#fbc7d4";
+            from = "#e55812";
+            to = "#e6aa1f";
           };
-          inactive.color = "#000000";
+          inactive.color = "#412e4e";
         };
 
         preset-column-widths = [
           {proportion = 1.0 / 3.0;}
           {proportion = 1.0 / 2.0;}
           {proportion = 2.0 / 3.0;}
+          {proportion = 1.0;}
         ];
         default-column-width = {proportion = 1.0 / 2.0;};
 
@@ -108,6 +111,45 @@ in {
           bottom = 0;
         };
       };
+
+      animations.shaders.window-resize = ''
+        vec4 resize_color(vec3 coords_curr_geo, vec3 size_curr_geo) {
+            vec3 coords_next_geo = niri_curr_geo_to_next_geo * coords_curr_geo;
+
+            vec3 coords_stretch = niri_geo_to_tex_next * coords_curr_geo;
+            vec3 coords_crop = niri_geo_to_tex_next * coords_next_geo;
+
+            // We can crop if the current window size is smaller than the next window
+            // size. One way to tell is by comparing to 1.0 the X and Y scaling
+            // coefficients in the current-to-next transformation matrix.
+            bool can_crop_by_x = niri_curr_geo_to_next_geo[0][0] <= 1.0;
+            bool can_crop_by_y = niri_curr_geo_to_next_geo[1][1] <= 1.0;
+
+            vec3 coords = coords_stretch;
+            if (can_crop_by_x)
+                coords.x = coords_crop.x;
+            if (can_crop_by_y)
+                coords.y = coords_crop.y;
+
+            vec4 color = texture2D(niri_tex_next, coords.st);
+
+            // However, when we crop, we also want to crop out anything outside the
+            // current geometry. This is because the area of the shader is unspecified
+            // and usually bigger than the current geometry, so if we don't fill pixels
+            // outside with transparency, the texture will leak out.
+            //
+            // When stretching, this is not an issue because the area outside will
+            // correspond to client-side decoration shadows, which are already supposed
+            // to be outside.
+            if (can_crop_by_x && (coords_curr_geo.x < 0.0 || 1.0 < coords_curr_geo.x))
+                color = vec4(0.0);
+            if (can_crop_by_y && (coords_curr_geo.y < 0.0 || 1.0 < coords_curr_geo.y))
+                color = vec4(0.0);
+
+            return color;
+        }
+      '';
+
       binds = with config.lib.niri.actions; let
         set-volume = spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@";
         brillo = spawn "${pkgs.brillo}/bin/brillo" "-q" "-u" "300000";
