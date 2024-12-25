3e7f1eda1d859321513c75aacd2fa785712f1409
Braian A. Diez <bdiez19@gmail.com>
2024-12-25 14:08:18 -0300

    fix: things

--- home/editors/zed/default.nix
+++ home/editors/zed/default.nix
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  zed-fhs = pkgs.buildFHSUserEnv {
-    name = "zed";
-    targetPkgs = pkgs:
-      with pkgs; [
-        zed-editor
-      ];
-    runScript = "zeditor";
-  };
-  jsonGenerator = lib.generators.toJSON {};
-in {
-||||||| parent of 4679ac2 (fix: improved zed)
-{
-  pkgs,
-  ...
-}: let
-  zed-fhs = pkgs.buildFHSUserEnv {
-    name = "zed";
-    targetPkgs = pkgs:
-      with pkgs; [
-        zed-editor
-      ];
-    runScript = "zeditor";
-  };
-in {
-=======
 {pkgs, ...}: {
->>>>>>> 4679ac2 (fix: improved zed)
   home.packages = with pkgs; [
     nodejs
     biome
