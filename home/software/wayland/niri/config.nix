efc672c97d31c118f834494e8639aaf1aaf61a97
Braian A. Diez <bdiez19@gmail.com>
2025-01-08 16:20:33 -0300

    fix: niri binds and zeditor

--- home/software/wayland/niri/config.nix
+++ home/software/wayland/niri/config.nix
@@ -27,8 +27,6 @@ in {
         (makeCommand "hyprlock")
         (makeCommand "wl-paste --type image --watch cliphist store")
         (makeCommand "wl-paste --type text --watch cliphist store")
-        (makeCommand "swww-daemon")
-        (makeCommand "eww daemon & eww open bar")
       ];
       input = {
         keyboard.xkb.layout = "latam";
@@ -99,39 +97,39 @@ in {
 
       animations.shaders.window-resize = ''
         vec4 resize_color(vec3 coords_curr_geo, vec3 size_curr_geo) {
-            vec3 coords_next_geo = niri_curr_geo_to_next_geo * coords_curr_geo;
+          vec3 coords_next_geo = niri_curr_geo_to_next_geo * coords_curr_geo;
 
-            vec3 coords_stretch = niri_geo_to_tex_next * coords_curr_geo;
-            vec3 coords_crop = niri_geo_to_tex_next * coords_next_geo;
+          vec3 coords_stretch = niri_geo_to_tex_next * coords_curr_geo;
+          vec3 coords_crop = niri_geo_to_tex_next * coords_next_geo;
 
-            // We can crop if the current window size is smaller than the next window
-            // size. One way to tell is by comparing to 1.0 the X and Y scaling
-            // coefficients in the current-to-next transformation matrix.
-            bool can_crop_by_x = niri_curr_geo_to_next_geo[0][0] <= 1.0;
-            bool can_crop_by_y = niri_curr_geo_to_next_geo[1][1] <= 1.0;
+          // We can crop if the current window size is smaller than the next window
+          // size. One way to tell is by comparing to 1.0 the X and Y scaling
+          // coefficients in the current-to-next transformation matrix.
+          bool can_crop_by_x = niri_curr_geo_to_next_geo[0][0] <= 1.0;
+          bool can_crop_by_y = niri_curr_geo_to_next_geo[1][1] <= 1.0;
 
-            vec3 coords = coords_stretch;
-            if (can_crop_by_x)
-                coords.x = coords_crop.x;
-            if (can_crop_by_y)
-                coords.y = coords_crop.y;
+          vec3 coords = coords_stretch;
+          if (can_crop_by_x)
+              coords.x = coords_crop.x;
+          if (can_crop_by_y)
+              coords.y = coords_crop.y;
 
-            vec4 color = texture2D(niri_tex_next, coords.st);
+          vec4 color = texture2D(niri_tex_next, coords.st);
 
-            // However, when we crop, we also want to crop out anything outside the
-            // current geometry. This is because the area of the shader is unspecified
-            // and usually bigger than the current geometry, so if we don't fill pixels
-            // outside with transparency, the texture will leak out.
-            //
-            // When stretching, this is not an issue because the area outside will
-            // correspond to client-side decoration shadows, which are already supposed
-            // to be outside.
-            if (can_crop_by_x && (coords_curr_geo.x < 0.0 || 1.0 < coords_curr_geo.x))
-                color = vec4(0.0);
-            if (can_crop_by_y && (coords_curr_geo.y < 0.0 || 1.0 < coords_curr_geo.y))
-                color = vec4(0.0);
+          // However, when we crop, we also want to crop out anything outside the
+          // current geometry. This is because the area of the shader is unspecified
+          // and usually bigger than the current geometry, so if we don't fill pixels
+          // outside with transparency, the texture will leak out.
+          //
+          // When stretching, this is not an issue because the area outside will
+          // correspond to client-side decoration shadows, which are already supposed
+          // to be outside.
+          if (can_crop_by_x && (coords_curr_geo.x < 0.0 || 1.0 < coords_curr_geo.x))
+              color = vec4(0.0);
+          if (can_crop_by_y && (coords_curr_geo.y < 0.0 || 1.0 < coords_curr_geo.y))
+              color = vec4(0.0);
 
-            return color;
+          return color;
         }
       '';
 
@@ -163,7 +161,7 @@ in {
         "Mod+Return".action = spawn "${
           inputs.ghostty.packages.${pkgs.system}.default
         }/bin/ghostty";
-        "Ctrl+Alt+L".action = spawn "pgrep hyprlock || hyprlock";
+        "Ctrl+Alt+L".action = spawn "sh -c pgrep hyprlock || hyprlock";
 
         "Mod+U".action = spawn "XDG_CURRENT_DESKTOP=gnome gnome-control-center";
 
@@ -183,12 +181,12 @@ in {
 
         "Mod+H".action = focus-column-left;
         "Mod+L".action = focus-column-right;
-        "Mod+J".action = focus-workspace-down;
-        "Mod+K".action = focus-workspace-up;
+        "Mod+J".action = focus-window-or-workspace-down;
+        "Mod+K".action = focus-window-or-workspace-up;
         "Mod+Left".action = focus-column-left;
         "Mod+Right".action = focus-column-right;
-        "Mod+Down".action = focus-window-down;
-        "Mod+Up".action = focus-window-up;
+        "Mod+Down".action = focus-workspace-down;
+        "Mod+Up".action = focus-workspace-up;
 
         "Mod+Shift+H".action = move-column-left;
         "Mod+Shift+L".action = move-column-right;
